3866c6984084e7d94aab30af83fb85829b207b16
Vlad Pereskokov <pereskokow@gmail.com>
2016-02-13 14:18:29 +0300

    add constants, new method to change interface color theme

--- app/src/main/java/casak/ru/tabs/TabsActivity.java
+++ app/src/main/java/casak/ru/tabs/TabsActivity.java
@@ -9,11 +9,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.TabLayout;
 import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
 import android.view.Window;
 
 public class TabsActivity extends ActionBarActivity {
     private ViewPager viewPager;
     private TabLayout tabLayout;
+
+    private static TypedArray COLOR_THEME_TOP250;
+    private static TypedArray COLOR_THEME_COMMING_SOON;
+    private static TypedArray COLOR_THEME_FAV;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,10 @@ public class TabsActivity extends ActionBarActivity {
         viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),
                 TabsActivity.this));
 
+        COLOR_THEME_TOP250 = getResources().obtainTypedArray(R.array.colorSetTop250);
+        COLOR_THEME_COMMING_SOON = getResources().obtainTypedArray(R.array.colorSetComingSoon);
+        COLOR_THEME_FAV = getResources().obtainTypedArray(R.array.colorSetFavorite);
+
         // Give the TabLayout the ViewPager
 
         tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
@@ -35,20 +45,34 @@ public class TabsActivity extends ActionBarActivity {
                     public void onTabSelected(TabLayout.Tab tab) {
                         super.onTabSelected(tab);
                         switch(tab.getPosition()){
-                            case 0:changeColor(getResources().obtainTypedArray(R.array.colorSetTop250)); break;
-                            case 1:changeColor(getResources().obtainTypedArray(R.array.colorSetComingSoon)); break;
-                            case 2:changeColor(getResources().obtainTypedArray(R.array.colorSetFavorite)); break;
-                            default:changeColor(getResources().obtainTypedArray(R.array.colorSetTop250)); break;
+                            case 0:changeColor(COLOR_THEME_TOP250); break;
+                            case 1:changeColor(COLOR_THEME_COMMING_SOON); break;
+                            case 2:changeColor(COLOR_THEME_FAV); break;
+                            default:changeColor(COLOR_THEME_TOP250); break;
                         }
                     }
                 });
     }
 
     private void changeColor(TypedArray colors){
-        getWindow().setStatusBarColor(colors.getColor(1,0));
+        changeInterfaceHeadColorTheme(
+                colors.getColor(0,0),
+                colors.getColor(1,0),
+                colors.getColor(2,0),
+                colors.getColor(3,0)
+        );
+        /*getWindow().setStatusBarColor(colors.getColor(1,0));
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(colors.getColor(0, 0)));
         tabLayout.setBackgroundColor(colors.getColor(3, 0));
         tabLayout.setSelectedTabIndicatorColor(colors.getColor(2, 0));
-        getWindow().setNavigationBarColor(colors.getColor(3,0));
+        getWindow().setNavigationBarColor(colors.getColor(3,0));*/
+    }
+
+    private void changeInterfaceHeadColorTheme(Integer zero_color, Integer status_bar_color, Integer selected_tab_color, Integer background_color) {
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(zero_color));
+        getWindow().setStatusBarColor(status_bar_color);
+        tabLayout.setBackgroundColor(background_color);
+        tabLayout.setSelectedTabIndicatorColor(selected_tab_color);
+        getWindow().setNavigationBarColor(background_color);
     }
 }
